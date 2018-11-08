a16a62085a6d2286015e1251ecf6456243ba3613
James Hageman <jamesd@hageman.ca>
2018-11-08 16:40:10 -0500

    show in memory counter

--- hello.go
+++ hello.go
@@ -5,6 +5,23 @@ import (
 	"net/http"
 )
 
+var (
+	counter = make(chan func(count int))
+)
+
+func init() {
+	fmt.Println("Starting now-test...")
+	go func() {
+		var count int
+		for f := range counter {
+			count++
+			f(count)
+		}
+	}()
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, now-test!")
+	counter <- func(count int) {
+		fmt.Fprintf(w, "Hello, now-test! Invocation number %d.", count)
+	}
 }
